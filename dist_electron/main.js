8ee95cfc84394351784290a65d384a14643f5984
Paweł Kenig <pawel.kng@gmail.com>
2020-02-15 19:44:09 +0100

    some

--- dist_electron/main.js
+++ dist_electron/main.js
@@ -2,51 +2,81 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var electron_1 = require("electron");
 var path = require("path");
-var some_class_1 = require("./some.class");
-console.log(new some_class_1.SomeClass().getPoint());
+var url = require("url");
+//**For develop serve argument is attached
+var appArgs = process.argv.slice(1);
+var serve = appArgs.some(function (val) { return val === '--serve'; });
+var win = null;
+// ** Function which creates window
 function createWindow() {
     // Stwórz okno przeglądarki.
-    var win = new electron_1.BrowserWindow({
+    win = new electron_1.BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true,
+            allowRunningInsecureContent: true
         }
     });
-    //
     //** Life electron reload */
     var mainDirName = path.join(__dirname, '..');
-    console.log(process.argv, 'some argv');
-    require('electron-reload')(__dirname, {
-        electron: require(mainDirName + "/node_modules/electron"),
-        hardResetMethod: 'quit',
-        forceHardReset: true,
-        argv: [process.argv[1]] //here is passed path './src_electron/main.js
+    //** Serving dev or production mode */
+    if (serve) {
+        require('electron-reload')(__dirname, {
+            electron: require(mainDirName + "/node_modules/electron"),
+            hardResetMethod: 'quit',
+            forceHardReset: true,
+            argv: [process.argv[1], process.argv[2]] //here is passed path './src_electron/main.js [1] , serve[2]
+        });
+        //** DEV: Angular view from serve mode */
+        win.loadURL('http://localhost:4200');
+    }
+    else {
+        //** Prod: path to angular build file */
+        //  win.loadFile(path.join(mainDirName, 'dist/Printer-App/index.html'));
+        win.loadURL(url.format({
+            pathname: path.join(mainDirName, 'dist/Printer-App/index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    }
+    if (serve) {
+        win.webContents.openDevTools();
+    }
+    win.on('closed', function () {
+        // Dereference the window object, usually you would store window
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
     });
-    // and load the index.html of the app. ///
-    win.loadFile(path.join(mainDirName, 'dist/Printer-App/index.html'));
-    // win.loadURL(url.format({
-    //   pathname: path.join(__dirname, '/dist/Printer-App/index.html'),
-    //   protocol: 'file:',
-    //   slashes: true
-    // }));
-    //
-    win.webContents.openDevTools();
+    return win;
 }
-electron_1.app.whenReady().then(createWindow);
-// Quit when all windows are closed.
-electron_1.app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        electron_1.app.quit();
-    }
-});
-electron_1.app.on('activate', function () {
-    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+//** Run electron app */
+try {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    electron_1.app.on('ready', createWindow);
+    // Quit when all windows are closed.
+    electron_1.app.on('window-all-closed', function () {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            electron_1.app.quit();
+        }
+    });
+    electron_1.app.on('activate', function () {
+        // On OS X it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (win === null) {
+            createWindow();
+        }
+    });
+}
+catch (e) {
+    console.log(e);
+}
 electron_1.ipcMain.on('openModal', function (event, arg) {
-    console.log(arg, 'some snake was found in srg ');
+    console.log(arg, 'some snake was found a in srg sssa1');
 });
-//
 //# sourceMappingURL=main.js.map
--- src_electron/main.ts
+++ src_electron/main.ts
@@ -1,65 +1,97 @@
 
-import { app, BrowserWindow, ipcMain} from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from 'path';
-import {SomeClass} from './some.class';
+import { SomeClass } from './some.class';
+import * as url from 'url';
 
-console.log(new SomeClass().getPoint());
 
-function createWindow() {
+//**For develop serve argument is attached
+const appArgs = process.argv.slice(1);
+const serve = appArgs.some(val => val === '--serve');
+let win:BrowserWindow = null;
+
+
+// ** Function which creates window
+function createWindow():BrowserWindow {
   // Stwórz okno przeglądarki.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-     // allowRunningInsecureContent: true
+      allowRunningInsecureContent: true
     }
   });
-//
+
+
   //** Life electron reload */
-  const mainDirName = path.join(__dirname,'..');
-  console.log(process.argv,'some argv')
-  
-  require('electron-reload')(__dirname, {
-  
-     electron: require(`${mainDirName}/node_modules/electron`),
-     hardResetMethod: 'quit',
-     forceHardReset:true,
-     argv:[process.argv[1]] //here is passed path './src_electron/main.js
-  })
+  const mainDirName = path.join(__dirname, '..');
 
-  // and load the index.html of the app. ///
-     win.loadFile(path.join(mainDirName,'dist/Printer-App/index.html'));
+  //** Serving dev or production mode */
+  if (serve) {
+    require('electron-reload')(__dirname, {
+      electron: require(`${mainDirName}/node_modules/electron`),
+      hardResetMethod: 'quit',
+      forceHardReset: true,
+      argv: [process.argv[1], process.argv[2]] //here is passed path './src_electron/main.js [1] , serve[2]
+    })
 
-  // win.loadURL(url.format({
-  //   pathname: path.join(__dirname, '/dist/Printer-App/index.html'),
-  //   protocol: 'file:',
-  //   slashes: true
-  // }));
+    //** DEV: Angular view from serve mode */
+    win.loadURL('http://localhost:4200');
+  } else {
+    //** Prod: path to angular build file */
+  //  win.loadFile(path.join(mainDirName, 'dist/Printer-App/index.html'));
+    win.loadURL(url.format({
+      pathname: path.join(mainDirName, 'dist/Printer-App/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
 
-  //
+  if (serve) {
+    win.webContents.openDevTools()
+  }
 
-  win.webContents.openDevTools()
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store window
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
+  return win;
 }
 
+//** Run electron app */
+try {
 
-app.whenReady().then(createWindow)
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-})
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+  });
+
+} catch (e) {
+  console.log(e);
+}
 
 ipcMain.on('openModal', (event, arg) => {
-  console.log(arg, 'some snake was found in srg ');
+  console.log(arg, 'some snake was found a in srg sssa1');
 })
 
-//
